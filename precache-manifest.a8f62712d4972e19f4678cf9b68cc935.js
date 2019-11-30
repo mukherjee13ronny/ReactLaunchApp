faea7c982d70c6ae2a8ff6ee852f34310a41dd14
unknown <mukherjee13ronny@gmail.com>
2019-12-01 02:19:58 +0530

    Updates

---
+++ precache-manifest.a8f62712d4972e19f4678cf9b68cc935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e4e012478395206be039b75f54d444",
+    "url": "/ReactLaunchApp/index.html"
+  },
+  {
+    "revision": "93d21c23172b60bc52e8",
+    "url": "/ReactLaunchApp/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "ca8e3bb165fd6311ca97",
+    "url": "/ReactLaunchApp/static/js/2.53e963d2.chunk.js"
+  },
+  {
+    "revision": "93d21c23172b60bc52e8",
+    "url": "/ReactLaunchApp/static/js/main.4f60b7ea.chunk.js"
+  },
+  {
+    "revision": "1966961f61f82d9d74ba",
+    "url": "/ReactLaunchApp/static/js/runtime~main.136f86cd.js"
+  }
+]);
